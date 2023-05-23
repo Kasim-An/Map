d5e766d9dbf7db24e5634b92c05152354a8e8835
Kasim-An <471658708@qq.com>
2023-05-23 17:16:09 -0400

    Missing timezone

--- public/app.js
+++ public/app.js
@@ -1,14 +1,16 @@
 new Vue({
   el: '#app',
-  data: {
-    map: null,
-    markers: new Map(),
-    searchResults: [],
-    searchAddress: '',
-    searchHistory: [],
-    selectedRecords: [],
-    latestSearchTimezone: '',
-    latestSearchLocalTime: ''
+  data() {
+    return {
+      map: null,
+      markers: [], // 修改为普通数组
+      searchResults: [],
+      searchAddress: '',
+      searchHistory: [],
+      selectedRecords: [],
+      latestSearchTimezone: '',
+      latestSearchLocalTime: ''
+    };
   },
   mounted() {
     this.initMap();
@@ -56,36 +58,36 @@ new Vue({
         }
       });
     },
-    addMarkersAndRecordSearch() {//////////////////////地图上的pin限制，删除时候移除
+    addMarkersAndRecordSearch() {
       this.searchResults.forEach(result => {
         const marker = new google.maps.Marker({
           position: result.geometry.location,
           map: this.map,
           title: result.formatted_address
         });
-
-        this.markers.push(marker); 
-
-        if (this.searchHistory.length >= 10) {
-          const oldestRecord = this.searchHistory.pop();
-          this.removeMarker(oldestRecord.id);
-        }
-
+    
         const record = {
           id: Date.now(),
           address: this.searchAddress,
-          result: result
+          result: result,
+          marker: marker // 将标记存储在记录中
         };
-        this.searchHistory.unshift(record); 
+        this.searchHistory.unshift(record);
+    
+        if (this.searchHistory.length > 10) {
+          const oldestRecord = this.searchHistory.pop();
+          oldestRecord.marker.setMap(null); // 移除最旧记录对应的标记
+        }
       });
     },
-    removeMarker(id) {
-      const markerIndex = this.markers.findIndex(marker => marker.id === id);
-      if (markerIndex !== -1) {
-        this.markers[markerIndex].setMap(null);
-        this.markers.splice(markerIndex, 1);
-      }
+    
+    
+    
+    removeMarker(marker) {
+      marker.setMap(null); // 将标记从地图上移除
     },
+    
+    
     clearMarkers() {
       this.markers.forEach(marker => marker.setMap(null));
       this.markers = [];
@@ -95,7 +97,7 @@ new Vue({
       if (recordIndex !== -1) {
         const record = this.searchHistory[recordIndex];
         this.searchHistory.splice(recordIndex, 1);
-        this.removeMarker(record.id);
+        this.removeMarker(record.marker); // 移除对应的标记
       }
     },
     deleteSelectedRecords() {
@@ -106,35 +108,9 @@ new Vue({
     },
     deleteAllRecords() {
       this.searchHistory.forEach(record => {
-        this.removeMarker(record.id);
+        this.removeMarker(record.marker); // 移除所有记录对应的标记
       });
       this.searchHistory = [];
     },
-    getTimezone(location) {
-      const timestamp = Math.floor(Date.now() / 1000);
-      const timeZoneUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${location.lat()},${location.lng()}&timestamp=${timestamp}&key=AIzaSyBvwwdYP6SsRlx_D8eKzy8C1gZKUWVIICE`;
-
-      // Make an AJAX request to get the time zone
-      // You need to replace YOUR_API_KEY with your actual Google Maps API key
-      return axios.get(timeZoneUrl)
-        .then(response => {
-          return response.data.timeZoneId;
-        })
-        .catch(error => {
-          console.error('Error getting time zone:', error);
-          return '';
-        });
-    },
-    getLocalTime(timezone) {
-      const options = {
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        timeZone: timezone
-      };
-
-      const localTime = new Date().toLocaleString('en-US', options);
-      return localTime;
-    }
   }
 });
